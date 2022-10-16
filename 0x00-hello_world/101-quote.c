168a9eb81ffa9f4799efd52bc3fd5f324f522338
0xAckerMan <joelkores1@gmail.com>
2022-10-17 01:39:36 +0300

    fprint

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 1 (sucess)
+ */
+
+int main(void)
+{
+fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
