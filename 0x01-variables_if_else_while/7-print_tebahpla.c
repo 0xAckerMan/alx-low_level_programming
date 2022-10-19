80f6d86405b735dc00ba2fba765b22944decb94c
0xAckerMan <joelkores1@gmail.com>
2022-10-19 22:42:43 +0300

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char lowAlpha;
+
+	for (lowAlpha = 'z'; lowAlpha >= 'a'; lowAlpha--)
+	{
+		putchar(lowAlpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
